12a1707ee8aa914fc5374c01f59d772e04f4fe1c
Jan Czekala <jan.czekala99@web.de>
2023-01-24 20:10:18 +0000

    Initial commit

---
+++ src/components.ts
@@ -0,0 +1,3 @@
+export { default as Auth } from "./components/Auth.astro"
+export { default as Signin } from "./components/Signin.astro"
+export { default as Signout } from "./components/Signout.astro"

e2b4318850ab2e6d21337485a5ffc0c1ef859d74
= <otterlord.dev@gmail.com>
2023-01-27 14:23:10 +0000

    Port the other fix

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,8 @@ import crypto from 'node:crypto'
 // Prior to Node 19.0.0, we need the following polyfills
 // This gives us at the very least support for Node ^17.4.0
 // See: https://github.com/nextauthjs/next-auth/issues/6417#issuecomment-1384660656
+// @ts-ignore
+if (!globalThis.crypto) globalThis.crypto = crypto
 // @ts-expect-error
 if (typeof globalThis.crypto.subtle === "undefined") globalThis.crypto.subtle = crypto.webcrypto.subtle
 if (typeof globalThis.crypto.randomUUID === "undefined") globalThis.crypto.randomUUID = crypto.randomUUID

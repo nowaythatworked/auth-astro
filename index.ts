5915c25dce6250427db2055e836c5437a3743619
nowaythatworked <jan.czekala99@web.de>
2023-03-20 17:50:23 +0100

    fix: no types for default import

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 import integration from './src/integration'
 
+export type Integration = typeof integration
 export default integration
--- module.d.ts
+++ module.d.ts
@@ -2,3 +2,8 @@ declare module "auth:config" {
   const config: import('./src/config').AstroAuthConfig;
   export default config;
 }
+
+declare module "auth-astro" {
+  const index: import('./index').Integration
+  export default index;
+}
